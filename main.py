2bb80e9d14bddeac62c69f48ccc42b461eb7d92b
KristenSoroka <>
2022-01-14 15:58:50 +0000

    saving to the hell of it

--- main.py
+++ main.py
@@ -6,14 +6,31 @@ print("Welcom To My First Python Game!")
 name = input('What is your name? ')
 age = int(input('What is your age? '))
 print(f'Hello, {name.upper()}! Welcome to your own adventure game! You are {age} years old.')
+#health of player
+health = 10
 
-#age verification
+#age verification using if/elif/else and nested statements
 if age >=18:
     print("You are old enough to play!")
-    
-    wants_to_play = input('Do you want to play? ')
+
+    wants_to_play = input('Do you want to play?(yes/no) ').lower()
     if wants_to_play == "yes":
         print("Lets Play!")
+        left_or_right = input("First choice....Left or Right?(left/right) ").lower()
+        if left_or_right == "left":
+          ans = input("Nice! You followed the path and reached a swamp.  Do you go across or go around?(across/around) ").lower()
+          if ans == "around":
+            print("You were attacked by swamp people and lost health")
+          elif ans == "across":
+            print("You were bitten by swamp snake and lost health")
+          else:
+            print("You Lose!")
+
+        else:
+          print("You fell down a hole and died...")
+
+    else:
+      print("Goodbye")
     
 elif age >= 13:
     print('You can play with parent supervision!')
