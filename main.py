67c7b9969b484e0ad313d44db19414cb9724a7a7
KristenSoroka <>
2022-01-14 17:31:16 +0000

    completed game for now. but want to add more layers and make it less wordy next time.  should also probably comment my code so I canunderstand it later

--- main.py
+++ main.py
@@ -14,51 +14,62 @@ name = input('What is your name? ')
 age = int(input('What is your age? '))
 print(f'Hello, {name.upper()}! Welcome to your own adventure game! You are {age} years old.')
 #health of player
-hearts = 5
+hearts = 10
 
 
 #age verification using if/elif/else and nested statements
 if age >=18:
     print("You are old enough to play!")
-
     wants_to_play = input('Do you want to play?(yes/no) ').lower()
     
     if wants_to_play == "yes":
         print("Lets Play!")
-
         print(f"You are starting with {hearts} hearts")
-        left_or_right = input("First choice....Left or Right?(left/right) ").lower()
+        
         # Left or Right Choice
+        left_or_right = input("First choice....Left or Right?(left/right) ").lower()
+        
         if left_or_right == "left":
           ans = input("Nice! You followed the path and reached a swamp.  Do you go across or go around?(across/around) ").lower()
           # Across or around the swamp choice
           if ans == "around":
-            print("You were attacked by swamp people and lost 3 hearts")
-            hearts -= 3
+            print("You were attacked by swamp people and lost 5 hearts")
+            hearts -= 5
             if hearts > 0:
                 print(f"You made it to the other side of the swamp alive. You have {hearts} hearts")
-                ans = input("You come to a fork in the road.  You can go left through the woods or right into the mountains.(left/right) ").lower()
-                if ans == "left":
-                    ans = input("you are traveling thru the forest and find anouther fork.  You can take the road less traveled(right) or the cleared path (left).  Which do you take? ").lower()
-                    if ans = "right":
-                      print()
-                  
-                  else
-                    hearts = 0
-                    print(f"You went into the mountains and died of frostbite during your journey. All remainng hearts are lost Hearts = {hearts}")
+                #mountians v froest
+                ans = input("You conintue along the path and find a fork in the road. You can go thru the forest or into the mountains. Where do you go?(forest/mountains) ")
+                if ans == "forest":
+                    print("You decided to go thru the forest and ate some poisonous berries and lost 5 hearts")
+                    hearts -= 5
+                    if hearts > 0:
+                        print("You made it home alive")
+                    else: 
+                        print("You died in the forest. You lose!")
+                else:
+                    print("You went into the mountains and died of frosbite.  You lose!")
+
             else:
-                print(f"Hearts = {hearts}, you died")
+                print("You lose!")
+
           elif ans == "across":
-            print("You went across but you were bitten by a swamp snake and lost 2 hearts")
-            hearts -= 2
+            print("You went across but you were bitten by a swamp snake and lost 4 hearts")
+            hearts -= 4
             if hearts > 0:
                 print(f"You made it to the other side of the swamp alive. You have {hearts} hearts")
-                print("You come to a fork in the road.  You can go left through the woods or right into the mountains. (left/right")
-            else:
-                print(f"Hearts = {hearts}, you die")
-            
+                #mountians v froest
+                ans = input("You conintue along the path and find a fork in the road. You can go thru the forest or into the mountains. Where do you go?(forest/mountains) ").lower()
+                if ans == "forest":
+                    print("You decided to go thru the forest and ate some poisonous berries and lost 5 hearts")
+                    hearts -= 5
+                    if hearts > 0:
+                        print("You made it home alive")
+                    else: 
+                        print("You died in the forest. You lose!")
+                else:
+                    print("You went into the mountains and died of frosbite.  You lose!")
           else:
-            print("You Lose!")
+            print("not a valid answer! start over")
 
         else:
           print("You fell down a hole and died immediately....")
